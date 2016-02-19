e93bc0faf43dc9c18858b9cdda431a89c7e33094
Henry Darnell <hjdarnel@uark.edu>
2016-02-19 11:32:06 -0600

    Initial commit

---
+++ app/src/test/java/edu/uark/csce/pizzaparty/uark_directory/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package edu.uark.csce.pizzaparty.uark_directory;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
