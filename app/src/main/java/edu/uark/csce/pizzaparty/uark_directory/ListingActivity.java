74583ce519b1d08aebccb03267b2f8baa01748c9
Silas Lankford <sblankfo@uark.edu>
2016-04-23 02:09:40 -0500

    Directory listview layout is mostly finished.

--- app/src/main/java/edu/uark/csce/pizzaparty/uark_directory/ListingActivity.java
+++ app/src/main/java/edu/uark/csce/pizzaparty/uark_directory/ListingActivity.java
@@ -1,7 +1,12 @@
 package edu.uark.csce.pizzaparty.uark_directory;
 
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class ListingActivity extends AppCompatActivity {
 
@@ -9,5 +14,24 @@ public class ListingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listing);
+
+        // Setting description box scroll movement
+        TextView descriptionBox = (TextView) findViewById(R.id.listing_description_box);
+        descriptionBox.setMovementMethod(new ScrollingMovementMethod());
+
+        LinearLayout layout = (LinearLayout) findViewById(R.id.scrollview_inner_layout);
+        for (int i = 0; i < 10; i++) {
+            ImageView imageView = new ImageView(this);
+            imageView.setId(i);
+            imageView.setPadding(2, 2, 2, 2);
+            imageView.setImageBitmap(BitmapFactory.decodeResource(
+                    getResources(), R.drawable.placeholder));
+            imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+
+            layout.addView(imageView);
+            imageView.getLayoutParams().height = 300;
+            imageView.getLayoutParams().width = 225;
+        }
+
     }
 }
